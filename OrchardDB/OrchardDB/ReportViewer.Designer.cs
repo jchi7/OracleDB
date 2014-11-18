3258ac4012f56b4cc55b7ce4dbfc4830f0e8c9f2
Jason <jason.chi103@gmail.com>
2014-11-18 05:19:32 -0800

    Add proof of concept report

--- OrchardDB/OrchardDB/ReportViewer.Designer.cs
+++ OrchardDB/OrchardDB/ReportViewer.Designer.cs
@@ -28,15 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.JCMA_PICKSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.JCMA_PICKSBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Location = new System.Drawing.Point(22, 12);
+            this.reportViewer1.AutoSize = true;
+            this.reportViewer1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "Picks";
+            reportDataSource1.Value = this.JCMA_PICKSBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "OrchardDB.FieldPerformance.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.Size = new System.Drawing.Size(444, 286);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // JCMA_PICKSBindingSource
+            // 
+            this.JCMA_PICKSBindingSource.DataSource = typeof(OrchardDB.JCMA_PICKS);
             // 
             // ReportViewer
             // 
@@ -47,12 +62,15 @@
             this.Name = "ReportViewer";
             this.Text = "ReportViewer";
             this.Load += new System.EventHandler(this.ReportViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.JCMA_PICKSBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource JCMA_PICKSBindingSource;
     }
 }
--- OrchardDB/OrchardDB/ReportViewer.cs
+++ OrchardDB/OrchardDB/ReportViewer.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
 
 namespace OrchardDB
 {
@@ -17,9 +19,14 @@ namespace OrchardDB
             InitializeComponent();
         }
 
+        private ODBEntities testEntities;
         private void ReportViewer_Load(object sender, EventArgs e)
         {
-
+            testEntities = new ODBEntities();
+            List<JCMA_PICKS> test = (from q in testEntities.JCMA_PICKS select q).ToList();
+            JCMA_PICKSBindingSource.DataSource = test;
+            ReportDataSource picks =  new ReportDataSource("Picks", JCMA_PICKSBindingSource);
+            reportViewer1.LocalReport.DataSources.Add(picks);
             this.reportViewer1.RefreshReport();
         }
     }
