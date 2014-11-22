0534dff0d4daa585723a1aee08d343c085988168
Jason <jason.chi103@gmail.com>
2014-11-21 20:50:21 -0800

    Update to the farm

--- OrchardDB/OrchardDB/AddNewForm2.cs
+++ OrchardDB/OrchardDB/AddNewForm2.cs
@@ -13,6 +13,7 @@ namespace OrchardDB
 {
     public partial class AddNewForm2 : Form
     {
+
         public AddNewForm2()
         {
             InitializeComponent();
@@ -21,49 +22,86 @@ namespace OrchardDB
         private void Save_button_Click(object sender, EventArgs e)
         {
             int b = 0;
+            List<string> errList = new List<string>();
             using (ODBEntities contextOdbEntities = new ODBEntities())
             {
                 if (AddDataGrid.Rows[0].Cells[0].ToString().Trim() != "")
                 {
                     for (int i = 0; i < AddDataGrid.RowCount - 1; i++)
                     {
-                        string farmName = "";
-                        string location = "";
-                        string ownerName = "";
-                        for (int j = 0; j < AddDataGrid.ColumnCount; j++)
+                        string farmname = AddDataGrid.Rows[0].Cells[0].ToString().Trim();
+                        if (contextOdbEntities.JCMA_FARM.Any(n => n.FARM_NAME != farmname))
                         {
-                            switch (j)
+                            string farmName = "";
+                            string location = "";
+                            string ownerName = "";
+                            List<string> insertedList = new List<string>();
+                            for (int j = 0; j < AddDataGrid.ColumnCount; j++)
                             {
-                                case 0:
-
-                                    farmName = AddDataGrid.Rows[i].Cells[j].Value.ToString().Trim();
-                                    break;
-                                case 1:
-                                    location = AddDataGrid.Rows[i].Cells[j].Value.ToString().Trim();
-                                    break;
-                                case 2:
-                                    ownerName = AddDataGrid.Rows[i].Cells[j].Value.ToString().Trim();
-                                    break;
-                                default:
-                                    MessageBox.Show("ERROR");
-                                    break;
+                                switch (j)
+                                {
+                                    case 0:
+                                        farmName = AddDataGrid.Rows[i].Cells[j].Value.ToString().Trim();
+                                        break;
+                                    case 1:
+                                        try
+                                        {
+                                            location = AddDataGrid.Rows[i].Cells[j].Value.ToString().Trim();
+                                        }
+                                        catch
+                                        {
+                                            location = "";
+                                        }
+                                        break;
+                                    case 2:
+                                        try
+                                        {
+                                            ownerName = AddDataGrid.Rows[i].Cells[j].Value.ToString().Trim();
+                                        }
+                                        catch (Exception)
+                                        {
+                                            ownerName = "";
+                                        }
+                                        break;
+                                    default:
+                                        MessageBox.Show("ERROR");
+                                        break;
+                                }
+                            }
+                            if (farmName == string.Empty)
+                                continue;
+                            try
+                            {
+                                b -= contextOdbEntities.INSERT_FARM(farmName, location, ownerName);
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Error in inserting");
                             }
                         }
-                        if (farmName == string.Empty)
-                            continue;
-                        try
+                        else
                         {
-                            b -= contextOdbEntities.INSERT_FARM(farmName, location, ownerName);
-                        }
-                        catch
-                        {
-                            MessageBox.Show("Error in inserting");
+                            errList.Add(farmname + " already exist in the database.");
                         }
                     }
                     MessageBox.Show(string.Format("{0} number of records has been saved.", b));
                 }
             }
+            if (errList.Count != 0)
+            {
+                string err = string.Empty;
+                foreach (string line in errList)
+                {
+                    err += line + "\n";
+                }
+                MessageBox.Show(err);
+            }
             Close();
+        }
+
+        private void Check_PK_Farm(string farmName)
+        {
+
         }
 
         private void Cancel_button_Click(object sender, EventArgs e)
